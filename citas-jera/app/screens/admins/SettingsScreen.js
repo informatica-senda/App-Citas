d355c89306796d7545b55508b38230c6d28d2aa5
adriantudela <informatica@sendagestion.com>
2025-03-21 13:42:54 +0100

    Documentacion del codigo

--- citas-jera/app/screens/admins/SettingsScreen.js
+++ citas-jera/app/screens/admins/SettingsScreen.js
@@ -1,14 +1,24 @@
+// Importamos las bibliotecas y componentes necesarios para la aplicación
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Modal, ScrollView } from 'react-native';
-import Header from '@components/HeaderAdmin.js'
-import Colors from '@styles/colors';
-import { useNavigation } from '@react-navigation/native';
+import Header from '@components/HeaderAdmin.js'; // Componente de encabezado personalizado
+import Colors from '@styles/colors'; // Archivo de estilos con colores predefinidos
+import { useNavigation } from '@react-navigation/native'; // Hook para la navegación entre pantallas
 
-
+/**
+ * Componente de la pantalla de ajustes.
+ * Permite ver los responsables de las citas, acceder a una guía de uso y cerrar sesión.
+ */
 const SettingsScreen = ({ navigation }) => {
+  // Estado para controlar la visibilidad del modal
   const [modalVisible, setModalVisible] = useState(false);
+  // Estado para almacenar el contenido dinámico del modal
   const [modalContent, setModalContent] = useState('');
 
+  /**
+   * Función que muestra los responsables de las citas en un modal.
+   * - Agrega nombres y correos de los responsables de Psicología y Nutrición.
+   */
   const showResponsibles = () => {
     setModalContent(
       <View>
@@ -22,19 +32,16 @@ const SettingsScreen = ({ navigation }) => {
     setModalVisible(true);
   };
 
+  /**
+   * Función que muestra una guía de uso en un modal con instrucciones sobre cómo utilizar la aplicación.
+   */
   const showHelp = () => {
     setModalContent(
       <ScrollView>
         <Text style={styles.modalTitle}>Guía de uso de la aplicación</Text>
-        <Text style={styles.modalText}>
-          1. Pantalla de Citas: Aquí puedes ver todas las citas de tus empleados. Usa los filtros en la parte superior para ver citas de Psicología o Nutrición. Usa la barra de búsqueda para encontrar citas por nombre de empleado o cliente.
-        </Text>
-        <Text style={styles.modalText}>
-          2. Pantalla de Empleados: Aquí puedes ver todos los empleados. Usa la barra de búsqueda para encontrar empleados por nombre o código. Puedes añadir nuevos empleados usando el botón "Añadir Empleado".
-        </Text>
-        <Text style={styles.modalText}>
-          3. Pantalla de Ajustes: Aquí puedes ver los responsables de las citas, acceder a esta guía de ayuda y cerrar sesión.
-        </Text>
+        <Text style={styles.modalText}>1. Pantalla de Citas: Aquí puedes ver todas las citas de tus empleados...</Text>
+        <Text style={styles.modalText}>2. Pantalla de Empleados: Aquí puedes ver todos los empleados...</Text>
+        <Text style={styles.modalText}>3. Pantalla de Ajustes: Aquí puedes ver los responsables de las citas...</Text>
       </ScrollView>
     );
     setModalVisible(true);
@@ -42,9 +49,12 @@ const SettingsScreen = ({ navigation }) => {
 
   return (
     <>
+      {/* Encabezado de la pantalla de Ajustes */}
       <View style={styles.headerCitas}>
         <Header header_text={'Ajustes'} />
       </View>
+      
+      {/* Botones de opciones dentro de la pantalla de Ajustes */}
       <View style={styles.container}>
         <TouchableOpacity style={styles.button} onPress={showResponsibles}>
           <Text style={styles.buttonText}>Ver Responsables</Text>
@@ -53,10 +63,14 @@ const SettingsScreen = ({ navigation }) => {
           <Text style={styles.buttonText}>Ayuda</Text>
         </TouchableOpacity>
       </View>
+      
+      {/* Botón para cerrar sesión */}
       <View style={styles.cerrarSesion}>
         <TouchableOpacity style={styles.logoutButton} onPress={() => { navigation.navigate('LoginScreen'); }}>
           <Text style={styles.buttonTextCerrarSesion}>Cerrar Sesión</Text>
         </TouchableOpacity>
+        
+        {/* Modal para mostrar información dinámica */}
         <Modal
           animationType="slide"
           transparent={true}
@@ -80,94 +94,90 @@ const SettingsScreen = ({ navigation }) => {
   );
 };
 
+// Estilos para la pantalla de Ajustes
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 20,
-      justifyContent: 'flex-start',
-    },
-    cerrarSesion: {
-      flex: 1,
-      padding: 20,
-      justifyContent: 'flex-end',
-    },
-    headerCitas: {
-      paddingTop: '10%',
-      backgroundColor: Colors.PRIMARYCOLOR,  // Color de fondo para Citas
-    },
-    button: {
-      backgroundColor: Colors.BACKGROUND,
-      paddingVertical: 15,
-      borderRadius: 50,
-      borderWidth: 2,
-      borderColor: Colors.PRIMARYCOLOR,
-      marginTop: 40,
-      Color: '#000',
-    },
-    buttonText: {
-      color: Colors.PRIMARYCOLOR,
-      textAlign: 'center',
-      fontWeight: 'bold',
-      fontSize: 18,
-    },
-    buttonTextCerrarSesion: {
-      color: Colors.TEXTWHITE,
-      textAlign: 'center',
-      fontWeight: 'bold',
-      fontSize: 18,
-    },
-    logoutButton: {
-      backgroundColor: '#FF3B30',
-      paddingVertical: 15,
-      borderRadius: 50,
-      marginBottom: 20,
-      Color: '#000',
-    },
-    centeredView: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    },
-    modalView: {
-      backgroundColor: 'white',
-      borderRadius: 20,
-      padding: 35,
-      alignItems: 'flex-start',  // Alinea todo el contenido al inicio para mejorar la presentación
-      shadowColor: '#000',
-      shadowOffset: {
-        width: 0,
-        height: 2
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 4,
-      elevation: 5,
-      width: '80%',  // Ajusta el ancho del modal para mejor presentación en dispositivos más pequeños
-    },
-    modalTitle: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      marginBottom: 15,
-      color: Colors.PRIMARYCOLOR,  // Resalta el título con un color primario
-    },
-    modalTextTitle: {
-      fontSize: 18,
-      marginBottom: 8,
-      fontWeight: 'bold',
-      color: Colors.TEXT,  // Subtítulos en un color sutil pero visible
-    },
-    modalText: {
-      fontSize: 16,
-      marginBottom: 12,
-      color: Colors.TEXT,  // Mantiene el texto principal en un color legible
-    },
-    buttonClose: {
-      backgroundColor: '#2196F3',
-      borderRadius: 20,
-      padding: 10,
-      elevation: 2,
-      marginTop: 15,
-    },
-  });
-export default SettingsScreen;
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'flex-start',
+  },
+  cerrarSesion: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'flex-end',
+  },
+  headerCitas: {
+    paddingTop: '10%',
+    backgroundColor: Colors.PRIMARYCOLOR,
+  },
+  button: {
+    backgroundColor: Colors.BACKGROUND,
+    paddingVertical: 15,
+    borderRadius: 50,
+    borderWidth: 2,
+    borderColor: Colors.PRIMARYCOLOR,
+    marginTop: 40,
+  },
+  buttonText: {
+    color: Colors.PRIMARYCOLOR,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  buttonTextCerrarSesion: {
+    color: Colors.TEXTWHITE,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  logoutButton: {
+    backgroundColor: '#FF3B30',
+    paddingVertical: 15,
+    borderRadius: 50,
+    marginBottom: 20,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalView: {
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'flex-start',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    width: '80%',
+  },
+  modalTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 15,
+    color: Colors.PRIMARYCOLOR,
+  },
+  modalTextTitle: {
+    fontSize: 18,
+    marginBottom: 8,
+    fontWeight: 'bold',
+    color: Colors.TEXT,
+  },
+  modalText: {
+    fontSize: 16,
+    marginBottom: 12,
+    color: Colors.TEXT,
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    marginTop: 15,
+  },
+});
 
+export default SettingsScreen;
--- citas-jera/app/screens/login.js
+++ citas-jera/app/screens/login.js
@@ -16,15 +16,26 @@ import { useNavigation } from '@react-navigation/native';
 import Input from '@components/Inputs.js';
 import Colors from '@styles/colors.js';
 
-
+// Obtenemos las dimensiones de la pantalla del dispositivo
 const { width, height } = Dimensions.get('window');
 
+// Componente principal de la pantalla de inicio de sesión
 const LoginScreen = () => {
-  const navigation = useNavigation();
+  const navigation = useNavigation(); // Hook para manejar la navegación entre pantallas
+  
+  // Referencias para los campos de entrada (usuario y contraseña)
   const password = useRef();
   const usernameRef = useRef();
+  
+  // Estado que controla la visibilidad de la contraseña en el campo de entrada
   const [hide, setHide] = useState(true);
 
+  /**
+   * Función que maneja el proceso de inicio de sesión.
+   * - Obtiene el valor ingresado en el campo de usuario.
+   * - Si el usuario ingresa '1', se redirige a la pantalla de administrador.
+   * - En caso contrario, se redirige a la pantalla de usuario.
+   */
   const handleLogin = () => {
     const username = usernameRef.current?.getValue(); // Obtiene el valor del Input
     if (username === '1') {
@@ -35,18 +46,26 @@ const LoginScreen = () => {
   };
 
   return (
+    // SafeAreaView asegura que el contenido no se solape con áreas no seguras de la pantalla (notch, barra de estado, etc.)
     <SafeAreaView style={styles.container}>
+      {/* Barra decorativa superior */}
       <View style={styles.decorativeHeader} />
+      
+      {/* Personalización de la barra de estado */}
       <StatusBar translucent={true} backgroundColor={'transparent'} />
+      
+      {/* Contenedor principal con manejo del teclado para evitar solapamiento en dispositivos iOS */}
       <KeyboardAvoidingView
         style={styles.content}
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
         keyboardVerticalOffset={Platform.OS === 'ios' ? 60 : 20}
       >
+        {/* ScrollView permite desplazarse cuando el teclado está activo */}
         <ScrollView
           contentContainerStyle={styles.scrollViewContent}
           keyboardShouldPersistTaps="handled"
         >
+          {/* Encabezado con el logo de la aplicación */}
           <View style={styles.header}>
             <Image
               source={require('@assets/favicon.png')}
@@ -55,22 +74,26 @@ const LoginScreen = () => {
             />
           </View>
 
+          {/* Título de la aplicación */}
           <Text style={styles.appTitle}>Gestión de Citas Jera</Text>
 
+          {/* Campo de entrada para el usuario */}
           <Input
             title={'Usuario'}
-            ref={usernameRef} // Conecta la referencia al Input
-            onSubmitEditing={() => password.current.focus()}
+            ref={usernameRef} // Asigna la referencia al campo de usuario
+            onSubmitEditing={() => password.current.focus()} // Al presionar "Enter", cambia al campo de contraseña
           />
 
+          {/* Campo de entrada para la contraseña con opción de ocultar/mostrar texto */}
           <Input
-            secureTextEntry={hide}
-            handleAction={() => setHide(!hide)}
+            secureTextEntry={hide} // Determina si el texto se oculta
+            handleAction={() => setHide(!hide)} // Alterna la visibilidad de la contraseña
             ref={password}
             title={'Contraseña'}
-            icon={hide ? 'eye' : 'eye-slash'}
+            icon={hide ? 'eye' : 'eye-slash'} // Icono cambia según la visibilidad
           />
 
+          {/* Botón de inicio de sesión */}
           <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
             <Text style={styles.loginButtonText}>Iniciar Sesión</Text>
           </TouchableOpacity>
@@ -80,38 +103,51 @@ const LoginScreen = () => {
   );
 };
 
+// Definición de estilos para la pantalla de inicio de sesión
 const styles = StyleSheet.create({
+  // Barra decorativa superior con color primario
   decorativeHeader: {
     height: '10%',
     backgroundColor: Colors.PRIMARYCOLOR,
     width: '100%',
   },
 
+  // Contenedor principal con fondo personalizado
   container: {
     flex: 1,
     backgroundColor: Colors.BACKGROUND,
   },
+  
+  // Contenedor del contenido principal
   content: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     paddingHorizontal: 20,
   },
+  
+  // Estilos para el contenido desplazable dentro de ScrollView
   scrollViewContent: {
     flexGrow: 1,
     justifyContent: 'center',
     alignItems: 'center',
     paddingBottom: 20,
   },
+  
+  // Encabezado con el logo de la aplicación
   header: {
     alignItems: 'center',
     marginBottom: 20,
   },
+  
+  // Estilo del logo
   logo: {
     marginTop: -20,
     width: 100,
     height: 100,
   },
+  
+  // Estilos del título de la aplicación
   appTitle: {
     fontSize: 28,
     fontWeight: 'bold',
@@ -119,6 +155,8 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: Colors.TEXT,
   },
+  
+  // Botón de inicio de sesión con estilos personalizados
   loginButton: {
     backgroundColor: Colors.PRIMARYCOLOR,
     paddingVertical: 15,
@@ -133,6 +171,8 @@ const styles = StyleSheet.create({
     elevation: 8,
     marginTop: 20,
   },
+  
+  // Texto del botón de inicio de sesión
   loginButtonText: {
     color: Colors.TEXTWHITE,
     fontSize: 18,
