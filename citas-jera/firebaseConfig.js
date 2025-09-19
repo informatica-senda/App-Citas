116183e611943a616a3ccd259239d8f2063005e2
informaticasendagestion <informaticasendagestion@gmail.com>
2025-09-19 10:56:14 +0000

    prueba error firebase

--- citas-jera/firebaseConfig.js
+++ citas-jera/firebaseConfig.js
@@ -2,18 +2,18 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Platform } from 'react-native';
+import { Platform, processColor } from 'react-native';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_APIKEY,
-  authDomain: process.env.EXPO_PUBLIC_AUTHDOMAIN,
-  projectId: process.env.EXPO_PUBLIC_PROJECTID,
-  storageBucket: process.env.EXPO_PUBLIC_STORAGEBUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGINGSENDERID,
-  appId: process.env.EXPO_PUBLIC_APPID
+  apiKey: env.EXPO_PUBLIC_APIKEY,
+  authDomain: env.EXPO_PUBLIC_AUTHDOMAIN,
+  projectId: env.EXPO_PUBLIC_PROJECTID,
+  storageBucket: env.EXPO_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: env.EXPO_PUBLIC_MESSAGINGSENDERID,
+  appId: env.EXPO_PUBLIC_APPID
 };
- 
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
